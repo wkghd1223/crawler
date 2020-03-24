390f030566eb6d40b9b7f4fbdf8f47bb26e8ac28
전현태 <wkghd1223@naver.com>
2020-03-24 16:41:12 +0900

    issue #1 delete opencv - part

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -9,7 +9,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 import numpy as np
 import cv2
-#iiiiiiiiiiiiiiiiiiiii
 from selenium.common.exceptions import JavascriptException
 import time
 import os
@@ -169,71 +168,6 @@ def getImage(keyword, limit):
     except TimeoutException:
         print("Time out")
 
+getImage("receipt test", 1)
 
-def point(path_of_img):
-    img = cv2.imread(path_of_img)
-    src = img.copy()
-    # point = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    size = 800.0
-    r = size / img.shape[0]
-    dim = (int(img.shape[1] * r), int(size))
-    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (1, 1), 7)
-    edged = cv2.Canny(gray, 75, 200)
-    cv2.imshow("edge", edged)
-    cv2.waitKey(0)
-
-    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-    print(cnts)
-    for c in cnts:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        if len(approx) == 4:
-            screenCnt = approx
-            break
-
-    cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 2)
-    cv2.imshow("asf", img)
-    cv2.waitKey(0)
-
-    rect = order_point(screenCnt.reshape(4, 2) / r)
-    (topLeft, topRight, bottomRight, bottomLeft) = rect
-
-    v1 = abs(bottomRight[0] - bottomLeft[0])
-    v2 = abs(topRight[0] - topLeft[0])
-    h1 = abs(topRight[1] - bottomRight[1])
-    h2 = abs(topLeft[1] - bottomLeft[1])
-    minWidth = min([v1, v2])
-    minHeight = min([h1, h2])
-
-    dst = np.float32([[0, 0], [minWidth - 1, 0], [minWidth - 1, minHeight - 1], [0, minHeight - 1]])
-
-    N = cv2.getPerspectiveTransform(rect, dst)
-
-    warped = cv2.warpPerspective(img, N, (int(minWidth), int(minHeight)))
-
-    cv2.imshow("asdff", warped)
-    cv2.waitKey(0)
-
-
-def order_point(pts):
-    rect = np.zeros((4, 2), dtype="float32")
-    s = pts.sum(axis=1)
-
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
-
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
-
-    return rect
-
-
-# src = cv2.imread('img/adffg.jpg')
-# point('img/adfewr.jpg')
-getImage("영수증", 50)## 5works 어깨깡패 전현태 매니저 사진을 키워드에 넣어봤습니다.
+## 5works 어깨깡패 전현태 매니저 사진을 키워드에 넣어봤습니다.
